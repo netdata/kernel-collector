a4ffc8956c5cfebac9bdadd1d5ea8631522855b2
James Mills <prologic@shortcircuit.net.au>
2020-02-24 10:13:06 +1000

    Added some logging for patching the kernel

--- .dockerfiles/patch_kernel.sh
+++ .dockerfiles/patch_kernel.sh
@@ -3,5 +3,10 @@
 cd /usr/src/linux || exit 1
 
 for patch in /usr/src/linux-"${KERNEL_VERSION}"-patches/*.diff.gz; do
-  zcat "${patch}" | patch -s -t -p 1
+  printf >&2 " Patching linux-%s with %s ... " "${KERNEL_VERSION}" "${patch}"
+  if zcat "${patch}" | patch -s -t -p 1; then
+    echo >&2 " OK"
+  else
+    echo >&2 " ERR"
+  fi
 done
