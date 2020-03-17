38931354cc7694ab1edf0980589d9bb05a729695
Thiago Marques <thiagoftsm@gmail.com>
2020-03-17 07:56:27 +0000

    redhat_kernel_detection: Change script
    This commit changes the script to also detect RH version and not only kernel version

--- tools/check-kernel-config.sh
+++ tools/check-kernel-config.sh
@@ -8,6 +8,19 @@ function get_kernel_version() {
   printf "%03d%03d%03d" "${p[0]}" "${p[1]}" "${p[2]}"
 }
 
+function get_rh_version() {
+  if [ ! -f /etc/redhat-release ] ; then
+     echo "000000000"
+     return
+  fi
+
+  r="$(cat /etc/redhat-release | cut -f 4 -d ' ')"
+
+  read -r -a p <<< "$(echo "${r}" | tr '.' ' ')"
+
+  printf "%03d%03d%03d" "${p[0]}" "${p[1]}" "${p[2]}"
+}
+
 if [ "$(uname -s)" != "Linux" ]; then
   echo >&2 "This does not appear to be a Linux system."
   exit 1
@@ -15,8 +28,8 @@ fi
 
 KERNEL_VERSION="$(uname -r)"
 
-if [ "$(get_kernel_version)" -lt 004014000 ]; then
-  echo >&2 "Your kernel appears to be older than 4.11. This may still work in some cases, but probably won't."
+if [ "$(get_kernel_version)" -lt 004014000 ] && [ "$(get_rh_version)" -lt 0070061810 ] ; then
+  echo >&2 "Your kernel appears to be older than 4.11 or you are using RH version older than 7.6.1810. This may still work in some cases, but probably won't."
 fi
 
 CONFIG_PATH=""
@@ -30,6 +43,8 @@ if [ -r /proc/config.gz ]; then
   CONFIG_PATH="/proc/config.gz"
 elif [ -r "/lib/modules/${KERNEL_VERSION}/source/.config" ]; then
   CONFIG_PATH="/lib/modules/${KERNEL_VERSION}/source/.config"
+elif [ -r "/lib/modules/${KERNEL_VERSION}.x86_64/source/.config" ]; then
+  CONFIG_PATH="/lib/modules/${KERNEL_VERSION}.x86_64/source/.config"
 elif [ -n "$(find /boot -name "config-${KERNEL_VERSION}*")" ]; then
   CONFIG_PATH="$(find /boot -name "config-${KERNEL_VERSION}*" | head -n 1)"
 fi
@@ -41,7 +56,7 @@ if [ -n "${CONFIG_PATH}" ]; then
     GREP='zgrep'
   fi
 
-  REQUIRED_CONFIG="KPROBES KPROBES_ON_FTRACE HAVE_KPROBES HAVE_KPROBES_ON_FTRACE KPROBE_EVENTS"
+  REQUIRED_CONFIG="KPROBES KPROBES_ON_FTRACE HAVE_KPROBES BPF BPF_SYSCALL BPF_JIT"
 
   for required_config in ${REQUIRED_CONFIG}; do
     if ! "${GREP}" -q "CONFIG_${required_config}=y" "${CONFIG_PATH}"; then
