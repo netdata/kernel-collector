6ea17501a9076ea6dd8d178f884535a216bc1de9
thiagoftsm <thiagoftsm@gmail.com>
2020-03-17 22:37:14 +0000

    redhat_kernel_detection: Change script (#102)
    * redhat_kernel_detection: Change script
    
    This commit changes the script to also detect RH version and not only kernel version
    
    * redhat_kernel_detection: Fix shellcheck error
    
    * Tester:
    
    This commit brings an update for the tester software
    
    * redhat_kernel_detection: update code with suggestion received

--- user/process_user.c
+++ user/process_user.c
@@ -212,9 +212,49 @@ int get_kernel_version()
    return ((int)(strtol(major, NULL, 10) * 65536) + (int)(strtol(minor, NULL, 10) * 256) + (int)strtol(patch, NULL, 10));
 }
 
+static int has_redhat_release()
+{
+    char out[256];
+    int major,minor;
+    FILE *fp = fopen("/etc/redhat-release", "r");
+    
+    if (fp) {
+        fread(out, sizeof(char), 255, fp);
+        fclose(fp);
+        char *end = strchr(out, '.');
+        char *start;
+        if (end) {
+            *end = 0x0;
+
+            if (end > out) {
+                start = end - 1;
+            }
+
+            major = strtol( start, NULL, 10);
+            start = ++end;
+
+            end++;
+            if(end) {
+                end = 0x00;
+                minor = strtol( start, NULL, 10);
+            } else {
+                minor = -1;
+            }
+        } else {
+            major = 0;
+            minor = -1;
+        }
+
+        return ((major<<8) + minor);
+    } else {
+        return -1;
+    }
+}
+
 static int has_ebpf_kernel_version(int version)
 {
-   return (version >= 264960);
+          //4.11.0 or RH > 7.5
+   return (version >= 264960 || has_redhat_release() >= 1797);
 }
 
 int has_condition_to_run(int version)
