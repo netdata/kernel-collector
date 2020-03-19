81aabbc7cd1004dcd1b4e933f6b261d38e811316
James Mills <prologic@shortcircuit.net.au>
2020-03-19 11:47:58 +1000

    Cleanup the scripts, patches and workflows to accomodate CentOS/RHEL special kernels with more consistency (#104)
    * Cleanup the scripts, patches and workflows to accomodate CentOS/RHEL special kernels with more consistency
    
    * Update release workflow to match build

--- .dockerfiles/patch_kernel.sh
+++ .dockerfiles/patch_kernel.sh
@@ -6,6 +6,11 @@ srcdir="${1:-/usr/src/linux}"
 
 cd "${srcdir}" || exit
 
+if [ ! -d /usr/src/linux-"${KERNEL_VERSION}"-patches ]; then
+  echo >&2 " No patches found for ${KERNEL_VERSION}"
+  exit 0
+fi
+
 for patch in /usr/src/linux-"${KERNEL_VERSION}"-patches/*.diff.gz; do
   printf >&2 " Patching linux-%s with %s ... " "${KERNEL_VERSION}" "${patch}"
   if zcat < "${patch}" | patch -p 1; then
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 log() {
   printf "%s\n" "${1}"
 }
@@ -14,15 +16,20 @@ fail() {
 }
 
 if [ "$#" -eq 0 ]; then
-  log "Usage: $(basename "$0") <kernel_version> [<libc>]"
+  log "Usage: $(basename "$0") <kernel_version> [<libc>] [<os>]"
   exit 1
 fi
 
 KERNEL_VERSION="${1}"
 LIBC="${2:-glibc}"
+OS="${3:-generic}"
 
-TAG="kernel-collector:$(echo "${KERNEL_VERSION}" | tr '.' '_')_${LIBC}"
+TAG="kernel-collector:$(echo "${KERNEL_VERSION}" | tr '.' '_')_${LIBC}_${OS}"
 
 git clean -d -f -x
-docker build -f Dockerfile."${LIBC}" -t "${TAG}" --build-arg KERNEL_VERSION="${KERNEL_VERSION}" ./ | tee prepare.log
-docker run -i -t --rm -v "$PWD":/kernel-collector -e DEBUG=1 "${TAG}" | tee build.log
+docker build -f Dockerfile."${LIBC}"."${OS}" -t "${TAG}" --build-arg KERNEL_VERSION="${KERNEL_VERSION}" ./ | tee prepare.log
+if [ -t 1 ]; then
+  docker run -i -t --rm -v "$PWD":/kernel-collector -w /kernel-collector -e DEBUG "${TAG}" | tee build.log
+else
+  docker run --rm -v "$PWD":/kernel-collector -w /kernel-collector -e DEBUG "${TAG}" | tee build.log
+fi
