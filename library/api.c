678d551a69d45ae0aeb87e2f95b08ddd9f16338e
Thiago Marques <thiagoftsm@gmail.com>
2020-05-20 19:27:59 +0000

    static_libraries: Remap functions

--- library/api.c
+++ library/api.c
@@ -7,8 +7,8 @@
 //#include "trace_helpers.h"
 //#include "api.h"
 
-//#include "bpf/bpf.h"
-//#include "bpf_load.h"
+#include "bpf/bpf.h"
+#include "bpf_load.h"
 
 int set_bpf_perf_event(int cpu, int map)
 {
@@ -45,3 +45,20 @@ void netdata_perf_loop_multi(int *pmu_fds, struct perf_event_mmap_page **headers
 //    perf_event_poller_multi(pmu_fds, headers, numprocs, print_bpf_output, killme, page_cnt);
 }
 */
+
+struct map_me_to_others {
+    int reserved;
+};
+
+//The next function was created for we have access to `bpf_*`
+//functions used inside it.
+void foce_map(int fd) {
+    struct map_me_to_others key = {}, next_key;
+    int values[2];
+    while (bpf_map_get_next_key(fd, &key, &next_key) == 0) {
+        bpf_map_lookup_elem(fd, &next_key, values);
+    }
+
+    int pmu_fd = 0;
+    assert(bpf_map_update_elem(1, &fd, &pmu_fd, BPF_ANY) == 0);
+}
