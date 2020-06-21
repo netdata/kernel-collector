02b91e70efe7c156a6b41cb0409413ac559a79c5
Thiago Marques <thiagoftsm@gmail.com>
2020-06-21 20:48:04 +0000

    libbpf_0_0_8: Brings new libbpf to repository

--- libbpf/travis-ci/vmtest/run_selftests.sh
+++ libbpf/travis-ci/vmtest/run_selftests.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-set -eux
+set -euxo pipefail
+
+test_progs() {
+	echo TEST_PROGS
+	./test_progs ${BLACKLIST:+-b$BLACKLIST} ${WHITELIST:+-t$WHITELIST}
+}
+
+test_maps() {
+	echo TEST_MAPS
+	# Allow failing on older kernels.
+	./test_maps
+}
+
+test_verifier() {
+	echo TEST_VERIFIER
+	./test_verifier
+}
 
 configs_path='libbpf/travis-ci/vmtest/configs'
 blacklist_path="$configs_path/blacklist/BLACKLIST-${KERNEL}"
 if [[ -s "${blacklist_path}" ]]; then
-	BLACKLIST=$(cat "${blacklist_path}" | tr '\n' ',')
+	BLACKLIST=$(cat "${blacklist_path}" | cut -d'#' -f1 | tr -s '[:space:]' ',')
 fi
 
 whitelist_path="$configs_path/whitelist/WHITELIST-${KERNEL}"
 if [[ -s "${whitelist_path}" ]]; then
-	WHITELIST=$(cat "${whitelist_path}" | tr '\n' ',')
+	WHITELIST=$(cat "${whitelist_path}" | cut -d'#' -f1 | tr -s '[:space:]' ',')
 fi
 
 cd libbpf/selftests/bpf
 
-echo TEST_PROGS
-./test_progs ${BLACKLIST:+-b$BLACKLIST} ${WHITELIST:+-t$WHITELIST}
+test_progs
+
+if [[ "${KERNEL}" == 'latest' ]]; then
+	test_maps
+	test_verifier
+fi
