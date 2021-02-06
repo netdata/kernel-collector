6d4a701af70f73b7f0bec6f21827b5f3895e201e
Thiago Marques <thiagoftsm@gmail.com>
2021-02-06 00:04:33 +0000

    ebpf_cachestat: Add cachestat collector to ebpf programs

---
+++ kernel/cachestat_kern.c
@@ -0,0 +1,137 @@
+#define KBUILD_MODNAME "process_kern"
+#include <linux/bpf.h>
+#include <linux/version.h>
+#include <linux/ptrace.h>
+
+#include <linux/threads.h>
+#include <linux/version.h>
+
+#include "bpf_helpers.h"
+#include "netdata_ebpf.h"
+
+struct bpf_map_def SEC("maps") cstat_global = {
+    .type = BPF_MAP_TYPE_HASH,
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(__u64),
+    .max_entries = NETDATA_CACHESTAT_END
+};
+
+struct bpf_map_def SEC("maps") cstat_pid = {
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0)) 
+    .type = BPF_MAP_TYPE_HASH,
+#else
+    .type = BPF_MAP_TYPE_PERCPU_HASH,
+#endif
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(netdata_cachestat_t),
+    .max_entries = NETDATA_CACHESTAT_END
+};
+
+/************************************************************************************
+ *
+ *                                 COMMON Section
+ *
+ ***********************************************************************************/
+
+static inline void netdata_update_u64(__u64 *res, __u64 value)
+{
+    __sync_fetch_and_add(res, value);
+    if ( (0xFFFFFFFFFFFFFFFF - *res) <= value) {
+        *res = value;
+    }
+}
+
+static inline void netdata_update_global(__u32 key, __u64 value)
+{
+    __u64 *res;
+    res = bpf_map_lookup_elem(&cstat_global, &key);
+    if (res)
+        netdata_update_u64(res, value) ;
+    else
+        bpf_map_update_elem(&cstat_global, &key, &value, BPF_NOEXIST);
+}
+
+/************************************************************************************
+ *
+ *                                   Probe Section
+ *
+ ***********************************************************************************/
+
+SEC("kprobe/add_to_page_cache_lru")
+int netdata_add_to_page_cache_lru(struct pt_regs* ctx)
+{
+    netdata_cachestat_t *fill, data = {};
+    netdata_update_global(NETDATA_KEY_CALLS_ADD_TO_PAGE_CACHE_LRU, 1);
+
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 pid = (__u32)(pid_tgid >> 32);
+    fill = bpf_map_lookup_elem(&cstat_pid ,&pid);
+    if (fill) {
+        netdata_update_u64(&fill->add_to_page_cache_lru, 1);
+    } else {
+        data.add_to_page_cache_lru = 1;
+        bpf_map_update_elem(&cstat_pid, &pid, &data, BPF_ANY);
+    }
+
+    return 0;
+}
+
+SEC("kprobe/mark_page_accessed")
+int netdata_mark_page_accessed(struct pt_regs* ctx)
+{
+    netdata_cachestat_t *fill, data = {};
+    netdata_update_global(NETDATA_KEY_CALLS_MARK_PAGE_ACCESSED, 1);
+
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 pid = (__u32)(pid_tgid >> 32);
+    fill = bpf_map_lookup_elem(&cstat_pid ,&pid);
+    if (fill) {
+        netdata_update_u64(&fill->mark_page_accessed, 1);
+    } else {
+        data.mark_page_accessed = 1;
+        bpf_map_update_elem(&cstat_pid, &pid, &data, BPF_ANY);
+    }
+
+    return 0;
+}
+
+SEC("kprobe/account_page_dirtied")
+int netdata_account_page_dirtied(struct pt_regs* ctx)
+{
+    netdata_cachestat_t *fill, data = {};
+    netdata_update_global(NETDATA_KEY_CALLS_ACCOUNT_PAGE_DIRTIED, 1);
+
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 pid = (__u32)(pid_tgid >> 32);
+    fill = bpf_map_lookup_elem(&cstat_pid ,&pid);
+    if (fill) {
+        netdata_update_u64(&fill->account_page_dirtied, 1);
+    } else {
+        data.account_page_dirtied = 1;
+        bpf_map_update_elem(&cstat_pid, &pid, &data, BPF_ANY);
+    }
+
+    return 0;
+}
+
+SEC("kprobe/mark_buffer_dirty")
+int netdata_mark_buffer_dirty(struct pt_regs* ctx)
+{
+    netdata_cachestat_t *fill, data = {};
+    netdata_update_global(NETDATA_KEY_CALLS_MARK_BUFFER_DIRTY, 1);
+
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 pid = (__u32)(pid_tgid >> 32);
+    fill = bpf_map_lookup_elem(&cstat_pid ,&pid);
+    if (fill) {
+        netdata_update_u64(&fill->mark_buffer_dirty, 1);
+    } else {
+        data.mark_buffer_dirty = 1;
+        bpf_map_update_elem(&cstat_pid, &pid, &data, BPF_ANY);
+    }
+
+    return 0;
+}
+
+char _license[] SEC("license") = "GPL";
+
