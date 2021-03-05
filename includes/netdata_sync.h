3068933e2fe9612b2b41fe98129fb4443c9b56e0
Thiago Marques <thiagoftsm@gmail.com>
2021-03-05 11:47:50 +0000

    ebpf_sync: add comment

--- includes/netdata_sync.h
+++ includes/netdata_sync.h
@@ -3,10 +3,12 @@
 #ifndef _NETDATA_SYNC_H_
 #define _NETDATA_SYNC_H_ 1
 
+// 
 enum sync_counters {
     NETDATA_KEY_SYNC_CALL,
     NETDATA_KEY_SYNC_ERROR,
 
+    // Keep this as last and don't skip numbers as it is used as element counter
     NETDATA_SYNC_END
 };
 
