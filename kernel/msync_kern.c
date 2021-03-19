a67f5b996ebbe9deb33f8aa1bca164c3b402f48c
thiagoftsm <thiagoftsm@gmail.com>
2021-03-19 11:12:37 +0000

    Ebpf extend sync (#207)
    Add monitoring for syncfs, fsync, fdatasync, sync_file_range, and msync

---
+++ kernel/msync_kern.c
@@ -0,0 +1,40 @@
+#define KBUILD_MODNAME "latency_tp_netdata"
+#include <linux/bpf.h>
+#include <linux/ptrace.h>
+
+#include "bpf_helpers.h"
+#include "netdata_ebpf.h"
+
+/************************************************************************************
+ *     
+ *                                 MAPS
+ *     
+ ***********************************************************************************/
+
+struct bpf_map_def SEC("maps") tbl_msync = {
+    .type = BPF_MAP_TYPE_HASH,
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(__u64),
+    .max_entries = NETDATA_SYNC_END
+};
+
+/************************************************************************************
+ *
+ *                               MSYNC SECTION
+ *
+ ***********************************************************************************/
+
+SEC("kprobe/" NETDATA_SYSCALL(msync))
+int netdata_syscall_sync(struct pt_regs* ctx)
+{
+    libnetdata_update_global(&tbl_msync, NETDATA_KEY_SYNC_CALL, 1);
+
+    return 0;
+}
+
+/************************************************************************************
+ *
+ *                             END MSYNC SECTION
+ *
+ ***********************************************************************************/
+
