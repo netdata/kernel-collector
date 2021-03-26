d6b80d3b0d22ccc956f12cf60dceadf5acb44314
thiagoftsm <thiagoftsm@gmail.com>
2021-03-26 11:08:48 +0000

    Ebpf Directory Cache (#209)
    Add new eBPF program to monitor operate system directory cache.

---
+++ includes/netdata_dc.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef _NETDATA_DIRECTORY_CACHE_H_
+#define _NETDATA_DIRECTORY_CACHE_H_ 1
+
+#include <linux/sched.h>
+
+typedef struct netdata_dc_stat {
+    __u64 references;
+    __u64 slow;
+    __u64 missed;
+} netdata_dc_stat_t;
+
+enum directory_cache_counters {
+    NETDATA_KEY_DC_REFERENCE,
+    NETDATA_KEY_DC_SLOW,
+    NETDATA_KEY_DC_MISS,
+
+    // Keep this as last and don't skip numbers as it is used as element counter
+    NETDATA_DIRECTORY_CACHE_END
+};
+
+#endif /* _NETDATA_DIRECTORY_CACHE_H_ */
+
---
+++ kernel/dc_kern.c
@@ -0,0 +1,91 @@
+#define KBUILD_MODNAME "dc_kern"
+#include <linux/bpf.h>
+#include <linux/ptrace.h>
+
+#include "bpf_helpers.h"
+#include "netdata_ebpf.h"
+
+/************************************************************************************
+ *
+ *                                   Maps Section
+ *
+ ***********************************************************************************/
+
+struct bpf_map_def SEC("maps") dcstat_global = {
+    .type = BPF_MAP_TYPE_HASH,
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(__u64),
+    .max_entries = NETDATA_CACHESTAT_END
+};
+
+struct bpf_map_def SEC("maps") dcstat_pid = {
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0)) 
+    .type = BPF_MAP_TYPE_HASH,
+#else
+    .type = BPF_MAP_TYPE_PERCPU_HASH,
+#endif
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(netdata_dc_stat_t),
+    .max_entries = 100000
+};
+
+/************************************************************************************
+ *
+ *                                   Probe Section
+ *
+ ***********************************************************************************/
+
+SEC("kprobe/lookup_fast")
+int netdata_lookup_fast(struct pt_regs* ctx)
+{
+    netdata_dc_stat_t *fill, data = {};
+    libnetdata_update_global(&dcstat_global, NETDATA_KEY_DC_REFERENCE, 1);
+
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 pid = (__u32)(pid_tgid >> 32);
+    fill = bpf_map_lookup_elem(&dcstat_pid ,&pid);
+    if (fill) {
+        libnetdata_update_u64(&fill->references, 1);
+    } else {
+        data.references = 1;
+        bpf_map_update_elem(&dcstat_pid, &pid, &data, BPF_ANY);
+    }
+
+    return 0;
+}
+
+SEC("kretprobe/d_lookup")
+int netdata_d_lookup(struct pt_regs* ctx)
+{
+    netdata_dc_stat_t *fill, data = {};
+    libnetdata_update_global(&dcstat_global, NETDATA_KEY_DC_SLOW, 1);
+
+    int ret = PT_REGS_RC(ctx);
+
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 pid = (__u32)(pid_tgid >> 32);
+    fill = bpf_map_lookup_elem(&dcstat_pid ,&pid);
+    if (fill) {
+        libnetdata_update_u64(&fill->slow, 1);
+    } else {
+        data.slow = 1;
+        bpf_map_update_elem(&dcstat_pid, &pid, &data, BPF_ANY);
+    }
+
+    // file not found
+    if (ret == 0) {
+        libnetdata_update_global(&dcstat_global, NETDATA_KEY_DC_MISS, 1);
+        fill = bpf_map_lookup_elem(&dcstat_pid ,&pid);
+        if (fill) {
+            libnetdata_update_u64(&fill->missed, 1);
+        } else {
+            data.missed = 1;
+            bpf_map_update_elem(&dcstat_pid, &pid, &data, BPF_ANY);
+        }
+    }
+
+    return 0;
+}
+
+char _license[] SEC("license") = "GPL";
+
