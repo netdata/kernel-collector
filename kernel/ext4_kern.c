a350572e07d2aa737fc12284c206b561d166db6e
Thiago Marques <thiagoftsm@gmail.com>
2021-06-02 22:21:37 +0000

    ebpf_ext4: Change code to bring compatibility with oldest kernels

--- kernel/ext4_kern.c
+++ kernel/ext4_kern.c
@@ -83,25 +83,21 @@ static void netdata_ext4_store_bin(__u32 bin, __u32 selection)
 {
     __u64 *fill, data;
     __u32 idx = selection*NETDATA_FS_MAX_BINS + bin;
-    if ( idx >= NETDATA_FS_MAX_ELEMENTS)
+    if (idx >= NETDATA_FS_MAX_ELEMENTS)
         return;
 
     fill = bpf_map_lookup_elem(&tbl_ext4 ,&idx);
     if (fill) {
         libnetdata_update_u64(fill, 1);
-    } else {
-        data = 1;
-        bpf_map_update_elem(&tbl_ext4, &idx, &data, BPF_NOEXIST);
-    }
+		return;
+    } 
+
+    data = 1;
+    bpf_map_update_elem(&tbl_ext4, &idx, &data, BPF_ANY);
 }
 
-#if (LINUX_VERSION_CODE > KERNEL_VERSION(5,0,0))
-static int netdata_ext4_end(struct pt_regs *ctx, __u32 selection)
-#elif (LINUX_VERSION_CODE > KERNEL_VERSION(4,19,0)) 
-static __always_inline int netdata_ext4_end(struct pt_regs *ctx, __u32 selection)
-#else
-static inline int netdata_ext4_end(struct pt_regs *ctx, __u32 selection)
-#endif
+SEC("kretprobe/ext4_file_read_iter")
+int netdata_ret_ext4_ext4_file_read_iter(struct pt_regs *ctx)
 {
     __u64 *fill, data;
     __u64 pid_tgid = bpf_get_current_pid_tgid();
@@ -121,33 +117,87 @@ static inline int netdata_ext4_end(struct pt_regs *ctx, __u32 selection)
     // convert to microseconds
     data /= 1000;
     bin = libnetdata_select_idx(data, NETDATA_FS_MAX_BINS_POS);
-    netdata_ext4_store_bin(bin, selection);
+    netdata_ext4_store_bin(bin, NETDATA_KEY_CALLS_READ);
 
-    return 0;
-}
-
-SEC("kretprobe/ext4_file_read_iter")
-int netdata_ret_ext4_ext4_file_read_iter(struct pt_regs *ctx)
-{
-    return netdata_ext4_end(ctx, NETDATA_KEY_CALLS_READ);
+	return 0;
 }
 
 SEC("kretprobe/ext4_file_write_iter")
 int netdata_ret_ext4_file_write_iter(struct pt_regs *ctx)
 {
-    return netdata_ext4_end(ctx, NETDATA_KEY_CALLS_WRITE);
+    __u64 *fill, data;
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 bin, pid = (__u32)(pid_tgid >> 32);
+
+    fill = bpf_map_lookup_elem(&tmp_ext4 ,&pid);
+    if (!fill)
+        return 0;
+
+    data = bpf_ktime_get_ns() - *fill;
+    bpf_map_delete_elem(&tmp_ext4, &pid);
+
+    // Skip entries with backward time
+    if ( (s64)data < 0)
+        return 0;
+
+    // convert to microseconds
+    data /= 1000;
+    bin = libnetdata_select_idx(data, NETDATA_FS_MAX_BINS_POS);
+    netdata_ext4_store_bin(bin, NETDATA_KEY_CALLS_WRITE);
+
+	return 0;
 }
 
 SEC("kretprobe/ext4_file_open")
 int netdata_ret_ext4_file_open(struct pt_regs *ctx)
 {
-    return netdata_ext4_end(ctx, NETDATA_KEY_CALLS_OPEN);
+    __u64 *fill, data;
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 bin, pid = (__u32)(pid_tgid >> 32);
+
+    fill = bpf_map_lookup_elem(&tmp_ext4 ,&pid);
+    if (!fill)
+        return 0;
+
+    data = bpf_ktime_get_ns() - *fill;
+    bpf_map_delete_elem(&tmp_ext4, &pid);
+
+    // Skip entries with backward time
+    if ( (s64)data < 0)
+        return 0;
+
+    // convert to microseconds
+    data /= 1000;
+    bin = libnetdata_select_idx(data, NETDATA_FS_MAX_BINS_POS);
+    netdata_ext4_store_bin(bin, NETDATA_KEY_CALLS_OPEN);
+
+	return 0;
 }
 
 SEC("kretprobe/ext4_sync_file")
 int netdata_ret_ext4_sync_file(struct pt_regs *ctx) 
 {
-    return netdata_ext4_end(ctx, NETDATA_KEY_CALLS_SYNC);
+    __u64 *fill, data;
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 bin, pid = (__u32)(pid_tgid >> 32);
+
+    fill = bpf_map_lookup_elem(&tmp_ext4 ,&pid);
+    if (!fill)
+        return 0;
+
+    data = bpf_ktime_get_ns() - *fill;
+    bpf_map_delete_elem(&tmp_ext4, &pid);
+
+    // Skip entries with backward time
+    if ( (s64)data < 0)
+        return 0;
+
+    // convert to microseconds
+    data /= 1000;
+    bin = libnetdata_select_idx(data, NETDATA_FS_MAX_BINS_POS);
+    netdata_ext4_store_bin(bin, NETDATA_KEY_CALLS_SYNC);
+
+	return 0;
 }
 
 char _license[] SEC("license") = "GPL";
