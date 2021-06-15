f81f048b6f46ec5bb61785db2ba38171c923490c
Thiago Marques <thiagoftsm@gmail.com>
2021-06-15 00:33:19 +0000

    memory_limit: Add new table and update code to avoid store data when apps integration is disabled

--- kernel/cachestat_kern.c
+++ kernel/cachestat_kern.c
@@ -27,6 +27,13 @@ struct bpf_map_def SEC("maps") cstat_pid = {
     .max_entries = PID_MAX_DEFAULT
 };
 
+struct bpf_map_def SEC("maps") cstat_ctrl = {
+    .type = BPF_MAP_TYPE_ARRAY,
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(__u32),
+    .max_entries = NETDATA_CONTROLLER_END
+};
+
 /************************************************************************************
  *
  *                                   Probe Section
@@ -39,8 +46,14 @@ int netdata_add_to_page_cache_lru(struct pt_regs* ctx)
     netdata_cachestat_t *fill, data = {};
     libnetdata_update_global(&cstat_global, NETDATA_KEY_CALLS_ADD_TO_PAGE_CACHE_LRU, 1);
 
+    __u32 pid = 0;
+    __u32 *apps = bpf_map_lookup_elem(&cstat_ctrl ,&pid);
+    if (apps)
+        if (*apps == 0)
+            return 0;
+
     __u64 pid_tgid = bpf_get_current_pid_tgid();
-    __u32 pid = (__u32)(pid_tgid >> 32);
+    pid = (__u32)(pid_tgid >> 32);
     fill = bpf_map_lookup_elem(&cstat_pid ,&pid);
     if (fill) {
         libnetdata_update_u64(&fill->add_to_page_cache_lru, 1);
@@ -58,8 +71,14 @@ int netdata_mark_page_accessed(struct pt_regs* ctx)
     netdata_cachestat_t *fill, data = {};
     libnetdata_update_global(&cstat_global, NETDATA_KEY_CALLS_MARK_PAGE_ACCESSED, 1);
 
+    __u32 pid = 0;
+    __u32 *apps = bpf_map_lookup_elem(&cstat_ctrl ,&pid);
+    if (apps)
+        if (*apps == 0)
+            return 0;
+
     __u64 pid_tgid = bpf_get_current_pid_tgid();
-    __u32 pid = (__u32)(pid_tgid >> 32);
+    pid = (__u32)(pid_tgid >> 32);
     fill = bpf_map_lookup_elem(&cstat_pid ,&pid);
     if (fill) {
         libnetdata_update_u64(&fill->mark_page_accessed, 1);
@@ -77,8 +96,14 @@ int netdata_account_page_dirtied(struct pt_regs* ctx)
     netdata_cachestat_t *fill, data = {};
     libnetdata_update_global(&cstat_global, NETDATA_KEY_CALLS_ACCOUNT_PAGE_DIRTIED, 1);
 
+    __u32 pid = 0;
+    __u32 *apps = bpf_map_lookup_elem(&cstat_ctrl ,&pid);
+    if (apps)
+        if (*apps == 0)
+            return 0;
+
     __u64 pid_tgid = bpf_get_current_pid_tgid();
-    __u32 pid = (__u32)(pid_tgid >> 32);
+    pid = (__u32)(pid_tgid >> 32);
     fill = bpf_map_lookup_elem(&cstat_pid ,&pid);
     if (fill) {
         libnetdata_update_u64(&fill->account_page_dirtied, 1);
