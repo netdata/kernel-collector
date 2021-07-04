51ffa29a8f6b0e3f4b8448cd203932ab40eb6703
Thiago Marques <thiagoftsm@gmail.com>
2021-07-04 22:23:47 +0000

    update_scripts: Add helpers inside script to avoid names that won't be loaded

--- kernel/rename_binaries.sh
+++ kernel/rename_binaries.sh
@@ -5,10 +5,50 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     exit 1
 fi
 
+parse_kernel_version() {
+    R="${1}.${2}"
+
+    read -r -a P <<< "$(echo "${R}" | tr '.' ' ')"
+
+    printf "%03d%03d" "${P[0]}" "${P[1]}"
+}
+
+select_kernel_version() {
+    KVER=$(parse_kernel_version "${1}" "${2}")
+
+    VER5_11_0="005011"
+    VER5_10_0="005010"
+    VER4_18_0="004018"
+    VER4_17_0="004017"
+    VER4_15_0="004015"
+    VER4_11_0="004011"
+    VER3_10_0="003010"
+
+    if [ "${KVER}" -eq "${VER3_10_0}" ]; then
+        KSELECTED="3.10";
+    elif [ "${KVER}" -eq "${VER4_18_0}" ]; then
+        KSELECTED="4.18";
+    elif [ "${KVER}" -ge "${VER5_11_0}" ]; then
+        KSELECTED="5.11";
+    elif [ "${KVER}" -ge "${VER5_10_0}" ]; then
+        KSELECTED="5.10";
+    elif [ "${KVER}" -ge "${VER4_17_0}" ]; then
+        KSELECTED="4.17";
+    elif [ "${KVER}" -ge "${VER4_15_0}" ]; then
+        KSELECTED="4.15";
+    elif [ "${KVER}" -ge "${VER4_11_0}" ]; then
+        KSELECTED="4.11";
+    fi
+
+    echo "${KSELECTED}"
+}
+
 VER_MAJOR="$1"
 VER_MINOR="$2"
 OBJECTNAME="$3"
 NAME=${OBJECTNAME%_*}
 
-cp "r${NAME}_kern.o" "../rnetdata_ebpf_${NAME}.${VER_MAJOR}.${VER_MINOR}.o"
-cp "p${NAME}_kern.o" "../pnetdata_ebpf_${NAME}.${VER_MAJOR}.${VER_MINOR}.o"
+KNAME=$(select_kernel_version "${1}" "${2}")
+
+cp "r${NAME}_kern.o" "../rnetdata_ebpf_${NAME}.${KNAME}.o"
+cp "p${NAME}_kern.o" "../pnetdata_ebpf_${NAME}.${KNAME}.o"
