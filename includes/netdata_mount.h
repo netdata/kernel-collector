37d32170bce36192a0ec6a909cf02239f6e88bbb
Thiago Marques <thiagoftsm@gmail.com>
2021-07-21 17:22:21 +0000

    ebpf_mount: Add header

---
+++ includes/netdata_mount.h
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef _NETDATA_MOUNT_H_
+#define _NETDATA_MOUNT_H_ 1
+
+enum mount_counters {
+    NETDATA_KEY_MOUNT_CALL,
+    NETDATA_KEY_UMOUNT_CALL,
+    NETDATA_KEY_MOUNT_ERROR,
+    NETDATA_KEY_UMOUNT_ERROR,
+
+    NETDATA_MOUNT_END
+};
+
+#endif /* _NETDATA_MOUNT_H_ */
+
