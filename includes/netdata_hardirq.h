e338dfe7581a9cd17ad9978ac62aa88746838fe8
Uman Shahzad <uman@mslm.io>
2021-08-12 12:48:14 +0500

    hardirq: add tracepoints for statically known hardware IRQs.

--- includes/netdata_hardirq.h
+++ includes/netdata_hardirq.h
@@ -15,7 +15,6 @@ struct netdata_irq_handler_entry {
 };
 
 // /sys/kernel/debug/tracing/events/irq/irq_handler_exit/
-// https://elixir.bootlin.com/linux/latest/source/include/trace/events/block.h
 struct netdata_irq_handler_exit {
     u64 pad;                    // This is not used with eBPF
     int irq;                    // offset:8;       size:4; signed:1;
@@ -38,5 +37,49 @@ typedef struct hardirq_val {
     // identifies the IRQ with a human-readable string.
     char name[NETDATA_HARDIRQ_NAME_LEN];
 } hardirq_val_t;
+
+/************************************************************************************
+ *                                HARDIRQ STATIC
+ ***********************************************************************************/
+
+// all of the `irq_vectors` events, except `vector_*`, have the same format.
+// cat /sys/kernel/debug/tracing/available_events | grep 'irq_vectors' | grep -v ':vector_'
+struct netdata_irq_vectors_entry {
+    u64 pad;                    // This is not used with eBPF
+    int vector;                 // offset:8;       size:4; signed:1;
+};
+struct netdata_irq_vectors_exit {
+    u64 pad;                    // This is not used with eBPF
+    int vector;                 // offset:8;       size:4; signed:1;
+};
+
+// these represent static IRQs that aren't given an IRQ ID like the ones above.
+// they each require separate entry/exit tracepoints to track.
+enum netdata_hardirq_static {
+    NETDATA_HARDIRQ_STATIC_APIC_THERMAL,
+    NETDATA_HARDIRQ_STATIC_APIC_THRESHOLD,
+    NETDATA_HARDIRQ_STATIC_APIC_ERROR,
+    NETDATA_HARDIRQ_STATIC_APIC_DEFERRED_ERROR,
+    NETDATA_HARDIRQ_STATIC_APIC_SPURIOUS,
+    NETDATA_HARDIRQ_STATIC_FUNC_CALL,
+    NETDATA_HARDIRQ_STATIC_FUNC_CALL_SINGLE,
+    NETDATA_HARDIRQ_STATIC_RESCHEDULE,
+    NETDATA_HARDIRQ_STATIC_LOCAL_TIMER,
+    NETDATA_HARDIRQ_STATIC_IRQ_WORK,
+    NETDATA_HARDIRQ_STATIC_X86_PLATFORM_IPI,
+
+    // must be last; used as counter.
+    NETDATA_HARDIRQ_STATIC_END
+};
+
+typedef struct hardirq_static_val {
+    // incremental counter storing the total latency so far.
+    u64 latency;
+
+    // temporary timestamp stored at the IRQ entry handler, to be diff'd with a
+    // timestamp at the IRQ exit handler, to get the latency to add to the
+    // `latency` field.
+    u64 ts;
+} hardirq_static_val_t;
 
 #endif /* _NETDATA_HARDIRQ_H_ */
