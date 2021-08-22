20979be5dd076353cad51ddf7884f1a0a378bcb8
Thiago Marques <thiagoftsm@gmail.com>
2021-08-22 17:58:05 +0000

    bring_new_libbpf: Update fsync_kern.c

--- kernel/fsync_kern.c
+++ kernel/fsync_kern.c
@@ -1,6 +1,5 @@
-#define KBUILD_MODNAME "latency_tp_netdata"
+#define KBUILD_MODNAME "fsync_netdata"
 #include <linux/bpf.h>
-#include <linux/ptrace.h>
 
 #include "bpf_helpers.h"
 #include "netdata_ebpf.h"
@@ -11,12 +10,12 @@
  *     
  ***********************************************************************************/
 
-struct bpf_map_def SEC("maps") tbl_fsync = {
-    .type = BPF_MAP_TYPE_ARRAY,
-    .key_size = sizeof(__u32),
-    .value_size = sizeof(__u64),
-    .max_entries = NETDATA_SYNC_END
-};
+struct {
+        __uint(type, BPF_MAP_TYPE_ARRAY);
+        __type(key, __u32);
+        __type(value, __u64);
+        __uint(max_entries, NETDATA_SYNC_END);
+} tbl_fsync SEC(".maps");
 
 /************************************************************************************
  *
