b0969a3507e9eafed38076a5c4e3c2e945c21002
Uman Shahzad <uman@mslm.io>
2021-08-29 15:58:20 +0500

    oomkill: use bpf_probe_read to read from kernel space.

--- kernel/oomkill_kern.c
+++ kernel/oomkill_kern.c
@@ -19,20 +19,18 @@ struct bpf_map_def SEC("maps") tbl_oomkill = {
 };
 
 SEC("kprobe/mark_oom_victim")
-int netdata_mark_oom_victim(
-    struct pt_regs *ctx,
-    struct task_struct *p
-) {
+int netdata_mark_oom_victim(struct pt_regs *ctx) {
     u32 key;
     netdata_oomkill_t val = {}, *valp;
 
-    key = p->pid;
+    struct task_struct *p = (struct task_struct *)PT_REGS_PARM1(ctx);
+    bpf_probe_read(&key, sizeof(key), &p->pid);
     valp = bpf_map_lookup_elem(&tbl_oomkill, &key);
     if (valp) {
         libnetdata_update_u32(&valp->killcnt, 1);
     } else {
         val.killcnt = 1;
-        bpf_probe_read(&val.comm, sizeof(val.comm), p->comm);
+        bpf_probe_read(val.comm, sizeof(val.comm), p->comm);
         bpf_map_update_elem(&tbl_oomkill, &key, &val, BPF_ANY);
     }
 
