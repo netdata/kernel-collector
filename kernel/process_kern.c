e68b95acaab64e701745a83712dd53b6b57eba81
Thiago Marques <thiagoftsm@gmail.com>
2021-10-10 15:57:17 +0000

    process_tracepoints: Remove unused helpers

--- kernel/process_kern.c
+++ kernel/process_kern.c
@@ -71,35 +71,6 @@ struct bpf_map_def SEC("maps") process_ctrl = {
 
 /************************************************************************************
  *     
- *                                 COMMON Section
- *     
- ***********************************************************************************/
-
-static unsigned int log2(unsigned int v)
-{
-    unsigned int r;
-    unsigned int shift;
-
-    r = (v > 0xFFFF) << 4; v >>= r;
-    shift = (v > 0xFF) << 3; v >>= shift; r |= shift;
-    shift = (v > 0xF) << 2; v >>= shift; r |= shift;
-    shift = (v > 0x3) << 1; v >>= shift; r |= shift;
-    r |= (v >> 1);
-
-    return r;
-}
-
-static unsigned int log2l(unsigned long v)
-{
-    unsigned int hi = v >> 32;
-    if (hi)
-        return log2(hi) + 32;
-    else
-        return log2(v);
-}
-
-/************************************************************************************
- *     
  *                                   PROCESS Section
  *     
  ***********************************************************************************/
