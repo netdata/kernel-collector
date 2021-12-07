e98edda187607d01262e33839ee3ff370e31ed07
Thiago Marques <thiagoftsm@gmail.com>
2021-12-07 21:03:49 +0000

    tracepoints: Add disk

--- includes/netdata_disk.h
+++ includes/netdata_disk.h
@@ -8,6 +8,21 @@
 #define NETDATA_DISK_MAX_HD 256L
 #define NETDATA_DISK_HISTOGRAM_LENGTH  (NETDATA_FS_MAX_BINS * NETDATA_DISK_MAX_HD)
 
+// Decode function extracted from: https://elixir.bootlin.com/linux/v5.10.8/source/include/linux/kdev_t.h#L7
+#define NETDATA_MINORBITS       20
+#define NETDATA_MINORMASK	((1U << NETDATA_MINORBITS) - 1)
+
+#define NETDATA_MAJOR(dev)	((unsigned int) ((dev) >> NETDATA_MINORBITS))
+#define NETDATA_MINOR(dev)	((unsigned int) ((dev) & NETDATA_MINORMASK))
+#define NETDATA_MKDEV(ma,mi)    (((ma) << MINORBITS) | (mi))
+
+static __always_inline u32 netdata_new_encode_dev(dev_t dev)
+{
+    unsigned major = NETDATA_MAJOR(dev);
+    unsigned minor = NETDATA_MINOR(dev);
+    return (minor & 0xff) | (major << 8) | ((minor & ~0xff) << 12);
+}
+
 // /sys/kernel/debug/tracing/events/block/block_rq_issue/
 struct netdata_block_rq_issue {
     u64 pad;                    // This is not used with eBPF
