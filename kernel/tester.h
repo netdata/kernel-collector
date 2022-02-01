d3b77593ce5664774dc8671b2e806f1553cd8127
Thiago Marques <thiagoftsm@gmail.com>
2022-02-01 14:37:07 +0000

    fix_cachestat_5_16: Add new kernel for enum

--- kernel/tester.h
+++ kernel/tester.h
@@ -43,7 +43,8 @@ enum netdata_ebpf_kernel_versions {
     NETDATA_EBPF_KERNEL_5_0  = 327680,  //  327680 = 5 * 65536 +  0 * 256
     NETDATA_EBPF_KERNEL_5_10 = 330240,  //  330240 = 5 * 65536 + 10 * 256
     NETDATA_EBPF_KERNEL_5_11 = 330496,  //  330240 = 5 * 65536 + 11 * 256
-    NETDATA_EBPF_KERNEL_5_15 = 331520   //  331520 = 5 * 65536 + 15 * 256
+    NETDATA_EBPF_KERNEL_5_15 = 331520,  //  331520 = 5 * 65536 + 15 * 256
+    NETDATA_EBPF_KERNEL_5_16 = 331776   //  331776 = 5 * 65536 + 16 * 256
 };
 
 /**
@@ -60,7 +61,8 @@ enum netdata_kernel_flag {
     NETDATA_V5_4  = 1 << 4,
     NETDATA_V5_10 = 1 << 5,
     NETDATA_V5_11 = 1 << 6,
-    NETDATA_V5_15 = 1 << 7
+    NETDATA_V5_15 = 1 << 7,
+    NETDATA_V5_16 = 1 << 8
 };
 
 enum netdata_kernel_counter {
@@ -72,6 +74,7 @@ enum netdata_kernel_counter {
     NETDATA_5_10,
     NETDATA_5_11,
     NETDATA_5_15,
+    NETDATA_5_16,
 
     NETDATA_VERSION_END
 };
