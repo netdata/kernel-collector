1c5d7e089a9758035d6160b33f08f0521209f811
Thiago Marques <thiagoftsm@gmail.com>
2022-02-13 19:16:03 +0000

    update_libbpf: Adjust hardirq preprocessor

--- kernel/hardirq_kern.c
+++ kernel/hardirq_kern.c
@@ -1,12 +1,13 @@
 #define KBUILD_MODNAME "hardirq_netdata"
-#include <linux/bpf.h>
 #include <linux/ptrace.h>
 #include <linux/genhd.h>
 
 #if (LINUX_VERSION_CODE > KERNEL_VERSION(4,11,0))
+#include <uapi/linux/bpf.h>
 #include "bpf_helpers.h"
 #include "bpf_tracing.h"
 #else
+#include <linux/bpf.h>
 #include "netdata_bpf_helpers.h"
 #endif
 #include "netdata_ebpf.h"
