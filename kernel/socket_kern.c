efc71d17ba95ac61c6c53e3a833d38cc894f3a46
Thiago Marques <thiagoftsm@gmail.com>
2022-11-11 00:43:20 +0000

    fix_nv: Update inet_csk_accept code

--- kernel/socket_kern.c
+++ kernel/socket_kern.c
@@ -146,6 +146,35 @@ struct bpf_map_def SEC("maps") socket_ctrl = {
 
 /************************************************************************************
  *
+ *                                 Common Section
+ *
+ ***********************************************************************************/
+
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5,6,0))
+static __always_inline u8 select_protocol(struct sock *sk)
+{
+    u8 protocol = 0;
+
+    int gso_max_segs_offset = offsetof(struct sock, sk_gso_max_segs);
+    int sk_lingertime_offset = offsetof(struct sock, sk_lingertime);
+
+    if (sk_lingertime_offset - gso_max_segs_offset == 4)
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        bpf_probe_read(&protocol, sizeof(u8), (void *)((long)&sk->sk_gso_max_segs) - 3);
+    else
+        bpf_probe_read(&protocol, sizeof(u8), (void *)((long)&sk->sk_wmem_queued) - 3);
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+        bpf_probe_read(&protocol, sizeof(u8), (void *)((long)&sk->sk_gso_max_segs) - 1);
+    else
+        bpf_probe_read(&protocol, sizeof(u8), (void *)((long)&sk->sk_wmem_queued) - 1);
+#endif
+
+    return protocol;
+}
+#endif // Kernel version 5.6.0
+
+/************************************************************************************
+ *
  *                                 General Socket Section
  *
  ***********************************************************************************/
@@ -153,6 +182,43 @@ struct bpf_map_def SEC("maps") socket_ctrl = {
 SEC("kretprobe/inet_csk_accept")
 int netdata_inet_csk_accept(struct pt_regs* ctx)
 {
+    netdata_passive_connection_t data = { };
+    netdata_passive_connection_idx_t idx = { };
+    struct sock *sk = (struct sock *)PT_REGS_RC(ctx);
+    u16 protocol;
+    if (!sk)
+        return 0;
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0))
+    protocol = 0;
+    bpf_probe_read(&protocol, sizeof(u16), &sk->sk_protocol);
+#else
+    protocol = (u16) select_protocol(sk);
+#endif
+
+    if (protocol != IPPROTO_TCP && protocol != IPPROTO_UDP)
+        return 0;
+
+    idx.protocol = protocol;
+    bpf_probe_read(&idx.port, sizeof(u16), &sk->__sk_common.skc_num);
+
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 tgid = (__u32)(pid_tgid);
+    __u32 pid = (__u32)(pid_tgid >> 32);
+
+    netdata_passive_connection_t *value = (netdata_passive_connection_t *)bpf_map_lookup_elem(&tbl_lports, &idx);
+    if (value) {
+        // Update PID, because process can die.
+        value->tgid = tgid;
+        value->pid = pid;
+        libnetdata_update_u64(&value->counter, 1);
+    } else {
+        data.tgid = tgid;
+        data.pid = pid;
+        data.counter = 1;
+        bpf_map_update_elem(&tbl_lports, &idx, &data, BPF_ANY);
+    }
+
     return 0;
 }
 
