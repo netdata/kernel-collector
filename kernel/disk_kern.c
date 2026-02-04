8bf9df2d0693580382209de89cc69f959d794d7b
thiagoftsm <thiagoftsm@gmail.com>
2026-02-04 16:36:59 +0000

    improve: Fix/Simplify disk

--- kernel/disk_kern.c
+++ kernel/disk_kern.c
@@ -17,40 +17,22 @@
 #include "bpf_helpers.h"
 #include "netdata_ebpf.h"
 
-/************************************************************************************
- *     
- *                                 MAPS
- *     
- ***********************************************************************************/
-
 NETDATA_BPF_PERCPU_HASH_DEF(tbl_disk_iocall, block_key_t, __u64, NETDATA_DISK_HISTOGRAM_LENGTH);
 NETDATA_BPF_HASH_DEF(tmp_disk_tp_stat, netdata_disk_key_t, __u64, 8192);
 NETDATA_BPF_ARRAY_DEF(disk_ctrl, __u32, __u64, NETDATA_CONTROLLER_END);
 
-/************************************************************************************
- *     
- *                                 DISK SECTION
- *     
- ***********************************************************************************/
-
-// Probably it is available after 4.13 only
-
 SEC("tracepoint/block/block_rq_issue")
 int netdata_block_rq_issue(struct netdata_block_rq_issue *ptr)
 {
-    // blkid generates these and we're not interested in them
     if (!ptr->dev)
         return 0;
 
-    netdata_disk_key_t key = {};
-    key.dev = ptr->dev;
-    key.sector = ptr->sector;
-
-    if (key.sector < 0)
-        key.sector = 0;
+    netdata_disk_key_t key = {
+        .dev = ptr->dev,
+        .sector = (ptr->sector < 0) ? 0 : ptr->sector
+    };
 
     __u64 value = bpf_ktime_get_ns();
-
     bpf_map_update_elem(&tmp_disk_tp_stat, &key, &value, BPF_ANY);
 
     libnetdata_update_global(&disk_ctrl, NETDATA_CONTROLLER_PID_TABLE_ADD, 1);
@@ -61,34 +43,29 @@ int netdata_block_rq_issue(struct netdata_block_rq_issue *ptr)
 SEC("tracepoint/block/block_rq_complete")
 int netdata_block_rq_complete(struct netdata_block_rq_complete *ptr)
 {
-    __u64 *fill;
-    netdata_disk_key_t key = {};
-    block_key_t blk = {};
-    key.dev = ptr->dev;
-    key.sector = ptr->sector;
+    netdata_disk_key_t key = {
+        .dev = ptr->dev,
+        .sector = (ptr->sector < 0) ? 0 : ptr->sector
+    };
 
-    if (key.sector < 0)
-        key.sector = 0;
-
-    fill = bpf_map_lookup_elem(&tmp_disk_tp_stat ,&key);
+    __u64 *fill = bpf_map_lookup_elem(&tmp_disk_tp_stat, &key);
     if (!fill)
         return 0;
 
-    // calculate and convert to microsecond
     u64 curr = bpf_ktime_get_ns();
-    __u64 data, *update;
     curr -= *fill;
     curr /= 1000;
 
-    blk.bin = libnetdata_select_idx(curr, NETDATA_FS_MAX_BINS_POS);
-    blk.dev = new_encode_dev(ptr->dev);
+    block_key_t blk = {
+        .bin = libnetdata_select_idx(curr, NETDATA_FS_MAX_BINS_POS),
+        .dev = netdata_new_encode_dev(ptr->dev)
+    };
 
-    // Update IOPS
-    update = bpf_map_lookup_elem(&tbl_disk_iocall ,&blk);
+    __u64 data = 1;
+    __u64 *update = bpf_map_lookup_elem(&tbl_disk_iocall, &blk);
     if (update) {
         libnetdata_update_u64(update, 1);
     } else {
-        data = 1;
         bpf_map_update_elem(&tbl_disk_iocall, &blk, &data, BPF_ANY);
     }
 
